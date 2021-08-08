6bb1b37498147ffa041fd67b7c233d143f730596
Hansanguk <hansanguk0222@gmail.com>
2021-08-08 16:52:58 +0900

    [feat] 스타일 관련 코드 추가

---
+++ src/style/styled.d.ts
@@ -0,0 +1,216 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    boxShadow: {
+      skyblue: string;
+      darkgray: string;
+      black: string;
+      bottomSkyBlue: string;
+      bottomRed: string;
+      thickDarkGray: string;
+    };
+
+    color: {
+      main: string;
+      lightBlack: string;
+      black1: string;
+      black2: string;
+      black3: string;
+      black4: string;
+      black5: string;
+      black6: string;
+      black7: string;
+      black8: string;
+      black9: string;
+
+      darkgray: string;
+
+      gray1: string;
+      gray2: string;
+      gray3: string;
+
+      lightGray1: string;
+      lightGray2: string;
+      lightGray3: string;
+
+      purple1: string;
+      purple2: string;
+      purple3: string;
+
+      white: string;
+      semiWhite: string;
+      modalWhite: string;
+
+      green1: string;
+      green2: string;
+      green3: string;
+
+      blue1: string;
+      blue2: string;
+      blue3: string;
+      blue4: string;
+      blue5: string;
+      blue6: string;
+      blue7: string;
+
+      red1: string;
+
+      yellow: string;
+      warningRed: string;
+      onConnect: string;
+
+      channelItemColor: string;
+      channelBorder: string;
+      threadHover: string;
+      googleColor: string;
+
+      darkBlue1: string;
+      darkBlue2: string;
+      lightBlue: string;
+      coralBlue: string;
+
+      jade: string;
+    };
+
+    size: {
+      font: {
+        xxxxxs: string;
+        xxxxs: string;
+        xxxs: string;
+        xxs: string;
+        xs: string;
+        s: string;
+        m: string;
+        l: string;
+        xl: string;
+        xxl: string;
+        xxxl: string;
+        bigContentLarge: string;
+        bigContentMedium: string;
+        bigContentSmall: string;
+        mediumContentLarge: string;
+        mediumContentMedium: string;
+        mediumContentSmall: string;
+        smallContentLarge: string;
+        smallContentMedium: string;
+        smallContentSmall: string;
+        cardContent: string;
+        veryBigContentLarge: string;
+        veryBigContentMedium: string;
+        veryBigContentSmall: string;
+        influenceContent: string;
+      };
+      padding: {
+        fourSide: {
+          productApplicationRemoveButton: string;
+          gridCardItem: string;
+        };
+        horizontal: {
+          header: {
+            large: string;
+            small: string;
+          };
+          leftHeaderMenu: string;
+          carouselLarge: string;
+          carouselMedium: string;
+          carouselSmall: string;
+          productApplicationBottomContainer: string;
+          productApplicationButtonBox: string;
+          productApplicationLargeChoiceButton: string;
+          productApplicationMediumChoiceButton: string;
+          productApplicationSmallChoiceButton: string;
+          reviews: string;
+        };
+        vertical: {
+          container: string;
+          productApplicationBottomContainer: string;
+          productApplicationButtonBox: string;
+          productApplicationLargeChoiceButton: string;
+          productApplicationMediumChoiceButton: string;
+          productApplicationSmallChoiceButton: string;
+        };
+      };
+      width: {
+        headerLargeStyledA: string;
+        headerSmallStyledA: string;
+        contentLargeStyledA: string;
+        contentSmallStyledA: string;
+        headerLargeLogo: string;
+        headerSmallLogo: string;
+        leftHeaderMenuItem: string;
+        leftHeaderButtonLarge: string;
+        leftHeaderButtonSmall: string;
+        carouselLargeContainer: string;
+        carouselMediumContainer: string;
+        carouselSmallContainer: string;
+        carouselLargeButton: string;
+        carouselMediumButton: string;
+        carouselSmallButton: string;
+        tshirtLargeImg: string;
+        tshirtMediumImg: string;
+        tshirtSmallImg: string;
+        binLargeImg: string;
+        binMediumImg: string;
+        binSmallImg: string;
+        hamburgerLarge: string;
+        hamburgerMedium: string;
+        hamburgerSmall: string;
+        carouselLargeContainerThreeTimes: string;
+        carouselMediumContainerThreeTimes: string;
+        carouselSmallContainerThreeTimes: string;
+        gridCardImg: string;
+        gridLargeBrand: string;
+        gridMediumBrand: string;
+        gridSmallBrand: string;
+        youtubeLarge: string;
+        youtubeSmall: string;
+        brandImg: string;
+      };
+      height: {
+        headerLargeStyledA: string;
+        headerSmallStyledA: string;
+        contentLargeStyledA: string;
+        contentSmallStyledA: string;
+        headerLargeDiv: string;
+        headerSmallDiv: string;
+        leftHeaderMenuItem: string;
+        carouselLargeContainer: string;
+        carouselMediumContainer: string;
+        carouselSmallContainer: string;
+        carouselLargeButton: string;
+        carouselMediumButton: string;
+        carouselSmallButton: string;
+        tshirtLargeImg: string;
+        tshirtMediumImg: string;
+        tshirtSmallImg: string;
+        binLargeImg: string;
+        binMediumImg: string;
+        binSmallImg: string;
+        hamburgerLarge: string;
+        hamburgerMedium: string;
+        hamburgerSmall: string;
+        gridCardItem: string;
+        gridCardImg: string;
+        gridReviewItem: string;
+      };
+      margin: {
+        horizontal: {
+          carouselButton: string;
+          productApplicationRemoveButtonIcon: string;
+        };
+      };
+      border: {
+        radius: string;
+      };
+      shadow: {
+        blur: string;
+        spread: string;
+      };
+    };
+
+    fontWeight: {
+      bold: number;
+    };
+  }
+}
